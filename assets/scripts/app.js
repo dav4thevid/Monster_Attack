e1d0f630f95fd38b8140eb254927f5c3321a32f9
David <dav4thevid@gmail.com>
2020-05-18 00:30:56 +0100

    enhan:fixed input value error

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -204,6 +204,7 @@ let i = 0;
   
 }
 
+
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healPlayerHealth);
